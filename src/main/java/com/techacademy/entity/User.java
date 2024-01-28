ebdf3d272c43d7d45538773d065961a18711695e
matcha-373 <minami.yoshizawa@gmail.com>
2024-01-28 22:41:52 +0900

    Chapter11完了

--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -7,14 +7,17 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PreRemove;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email; // 追加
-import jakarta.validation.constraints.Max; // 追加
-import jakarta.validation.constraints.Min; // 追加
-import jakarta.validation.constraints.NotEmpty; // 追加
-import jakarta.validation.constraints.NotNull; // 追加
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.Length; // 追加
+import org.hibernate.validator.constraints.Length;
 
 import lombok.Data;
 
@@ -55,5 +58,20 @@ public class User {
     @Email // 追加
     @Length(max=50) // 追加
     private String email;
+
+ // ----- 追加ここから -----
+    @OneToOne(mappedBy="user")
+    private Authentication authentication;
+
+    /** レコードが削除される前に行なう処理 */
+    @PreRemove
+    @Transactional
+    private void preRemove() {
+        // 認証エンティティからuserを切り離す
+        if (authentication!=null) {
+            authentication.setUser(null);
+        }
+    }
+    // ----- 追加ここまで -----
 
 }
