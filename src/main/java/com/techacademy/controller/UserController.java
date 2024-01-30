17a05a4e92e54e237cb52d7101c8083949423cd1
matcha-373 <minami.yoshizawa@gmail.com>
2024-01-30 23:14:18 +0900

    課題提出

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -58,21 +58,34 @@ public class UserController {
 
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
-    public String getUser(@PathVariable("id") Integer id, Model model) {
+    public String getUser(@PathVariable("id") Integer id, Model model, User user) {
+        
         // Modelに登録
-        model.addAttribute("user", service.getUser(id));
+        if(id == null) {
+            model.addAttribute("user", user);
+        }else {
+            model.addAttribute("user", service.getUser(id));
+        }
+        
         // User更新画面に遷移
         return "user/update";
     }
 
     /** User更新処理 */
     @PostMapping("/update/{id}/")
-    public String postUser(User user) {
+    public String postUser(@PathVariable("id") Integer id, @Validated User user,BindingResult res, Model model) {
+
+        if(res.hasErrors()) {
+            // エラーあり
+            return getUser(null,model,user);
+        }
+
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
+    
 
     /** User削除処理 */
     @PostMapping(path="list", params="deleteRun")
